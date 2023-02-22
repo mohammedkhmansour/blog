da5518ffb5fabe0c4d0794c85aa8099bc0c2023d
mohammed khader abu mansour <mohammedmansour0387@gmail.com>
2023-02-22 13:26:03 +0200

    customize dashboard

---
+++ resources/views/layouts/admin/master.blade.php
@@ -0,0 +1,16 @@
+@include('layouts.admin.head')
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+@include('layouts.admin.header')
+
+  <!-- Main Sidebar Container -->
+@include('layouts.admin.sidebar')
+
+@yield('content')
+
+
+  <!-- /.content-wrapper -->
+@include('layouts.admin.footer')
+
+@include('layouts.admin.scripts')
